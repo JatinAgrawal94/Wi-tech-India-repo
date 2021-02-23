716657ccf6a4555a60977e5174eeb4b1e4ae130a
Jatin Agrawal <jatinagrawal0801@gmail.com>
2021-02-23 23:33:22 +0530

    temp

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json())
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
 
 // mongodb://localhost/witech
 mongoose.connect(`${process.env.MONGODB_URL}`,{
